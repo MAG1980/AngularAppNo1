ff489faca6b5d83aa52e7fa15d2d9e73d7822395
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-10-01 21:23:18 +0300

    Добавлена вёрстка меню. Шаблон приложения разбит на компоненты.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonsAreaComponent } from "./buttons-area/buttons-area.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecipesComponent, ButtonsAreaComponent, ShoppingListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/buttons-area/buttons-area.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons-area',
+  standalone: true,
+  imports: [],
+  templateUrl: './buttons-area.component.html',
+  styleUrl: './buttons-area.component.scss'
+})
+export class ButtonsAreaComponent {
+
+}
