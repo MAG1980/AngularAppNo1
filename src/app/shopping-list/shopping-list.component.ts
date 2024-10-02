30eed1ca5de4cb5047e1e59016338d0d6e11fbfa
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-10-02 19:06:53 +0300

    Настроен вывод данных в шаблон списка ингредиентов.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
 import { Ingredient } from "../shared/ingredient.model";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
   imports: [
-    ShoppingEditComponent
+    ShoppingEditComponent,
+    NgForOf
   ],
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.scss'
 })
 export class ShoppingListComponent {
-  ingredients: Ingredient[] = []
+  ingredients: Ingredient[] = [new Ingredient("Apples", 5), new Ingredient("Tomatoes", 10)];
 }
