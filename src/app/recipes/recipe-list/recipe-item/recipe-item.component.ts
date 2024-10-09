e8e172f55d054d22a2c3431c7a3adace3a6f0864
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-10-09 10:40:40 +0300

    Настроен вывод списка рецептов в цикле *ngFor.

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgForOf } from "@angular/common";
+import { Recipe } from "../../recipe.model";
 
 @Component({
   selector: 'app-recipe-item',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './recipe-item.component.html',
   styleUrl: './recipe-item.component.scss'
 })
 export class RecipeItemComponent {
-
+  @Input() recipe!: Recipe
 }
