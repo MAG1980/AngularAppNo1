98bffeb1e3562027aceb6330a2b1715f6b531ec0
MAG1980 <al-1980@inbox.ru>
2024-10-27 16:01:18 +0300

    Настроено отображение компонента-плейсхолдера до выбора рецепта путём определения компонента отображаемого для корневого пути вложенного маршрута ("").

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,15 @@ import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
 import { Routes } from "@angular/router";
 import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
 import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import {
+  RecipeDetailPlaceholderComponent
+} from "./recipes/recipe-detail/recipe-detail-placeholder/recipe-detail-placeholder.component";
 
 export const routes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
   {
     path: 'recipes', component: RecipesComponent, children: [
+      { path: '', component: RecipeDetailPlaceholderComponent },
       { path: ':id', component: RecipeDetailComponent },
       { path: ':id/edit', component: RecipeEditComponent }]
   },
---
+++ src/app/recipes/recipe-detail/recipe-detail-placeholder/recipe-detail-placeholder.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-detail-placeholder',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-detail-placeholder.component.html',
+  styleUrl: './recipe-detail-placeholder.component.scss'
+})
+export class RecipeDetailPlaceholderComponent {
+
+}
