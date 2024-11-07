f3c72c9b034aebfc7a3e29492f5f3bd4d50311bd
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-11-07 17:18:46 +0300

    Использован подход для работы с формами на основе шаблонов для добавления ингредиентов в список покупок. Настроена валидация. Применён предоставляемый Angular атрибут pattern, позволяющий выполнять валидацию с помощью регулярного выражения. Настроено отключение кнопки Submit, если данные не прошли валидацию.

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,33 +1,29 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { ShoppingListService } from "../shopping-list.service";
 import { Ingredient } from "../../shared/ingredient.model";
+import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './shopping-edit.component.html',
   styleUrl: './shopping-edit.component.scss'
 })
 export class ShoppingEditComponent {
-  @ViewChild('name') ingredientNameInput!: ElementRef;
-  @ViewChild('amount') ingredientAmountInput!: ElementRef;
 
-  constructor(private shoppingListService: ShoppingListService) { }
-
-  addIngredient() {
-    const nameInput = this.ingredientNameInput?.nativeElement as HTMLInputElement
-    const amountInput = this.ingredientAmountInput?.nativeElement as HTMLInputElement
-    this.shoppingListService.addIngredient(new Ingredient(nameInput.value, +amountInput.value))
-    nameInput.value = ''
-    amountInput.value = ''
+  constructor(private shoppingListService: ShoppingListService) {
   }
 
-  clearInputs() {
-    const nameInput = this.ingredientNameInput?.nativeElement as HTMLInputElement
-    const amountInput = this.ingredientAmountInput?.nativeElement as HTMLInputElement
-    nameInput.value = ''
-    amountInput.value = ''
+  addIngredient(form: NgForm) {
+    this.shoppingListService.addIngredient(new Ingredient(form.value.name.trim(), +form.value.amount))
+    form.reset()
+  }
+
+  clearInputs(form: NgForm) {
+    form.reset()
   }
 
   deleteItem() {
