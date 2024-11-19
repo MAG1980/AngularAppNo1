3ab1bdf2fb8abe008a99c5de061417c0c5ab7c2b
MAG1980 <76469986+MAG1980@users.noreply.github.com>
2024-11-19 19:28:56 +0300

    Исправлена ошибка в RecipeResolverService, приводившая к тому, что изменения в редактированных рецептах не сохранялись, из-за того, что resolver при переходе на страницу конкретного рецепта сразу загружал необновлённый список рецептов из FireBase.

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from "@angular/router";
 import { Recipe } from "./recipe.model";
 import { DataStorageService } from "../common/data-storage.service";
+import { RecipeService } from "./recipe.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,17 @@ import { DataStorageService } from "../common/data-storage.service";
 export class RecipeResolverService implements Resolve<Recipe[]> {
 
   constructor(
-    private dataStorageService: DataStorageService
+    private dataStorageService: DataStorageService,
+    private recipeService: RecipeService
   ) {
   }
 
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    //Подписка не требуется, т.к. выполняется Angular автоматически
-    return this.dataStorageService.fetchRecipes()
+    const recipes = this.recipeService.getRecipes()
+    if ( !recipes ) {
+      //Подписка не требуется, т.к. выполняется Angular автоматически
+      return this.dataStorageService.fetchRecipes()
+    }
+    return recipes
   }
 }
